70d2a3e251b65ae55ea71cef737b8f85f60f6e4d
Renato Ribeiro <joserenato2014@hotmail.com>
2024-10-14 07:31:10 -0300

    Update questao15.c

--- questao15.c
+++ questao15.c
@@ -10,5 +10,6 @@ int main(){
 //pois x+3, *(x+3), *(x+2)+3 fornecem o mesmo resultado, mesmo que escritos de maneira diferente;
 //x+3 é o mesmo que x[3], endereço do primeiro elemento da quarta linha;
 //*(x+3): Isso é o mesmo que x[3]
-//*(x+2)+3 é equivalente a x[2][3]. Já esse não pega o endereço da quarta linha, pega da terceira, e também
-//da quarta coluna, entretanto são apenas 3. Por isso apresenta o endereço do que em tese estaria nessa posição, que nesse caso já é a quarta linha.
+//x[i][j] -> i fornece o salto em linhas(grupos) e [j] forec e o salto em colunas(individual).
+//*(*(x+2)+3) é equivalente a x[2][3]. Já esse não pega o endereço da quarta linha, pega da terceira.E deveria pegar um elemento
+//da quarta coluna, entretanto são apenas 3 elementos por linha. Por isso apresenta o endereço do que em tese estaria nessa posição no endereço de memória, que nesse caso já é a quarta linha.
