d20b7941c6cf9cb3df2513bc69b7070adf7ab815
Renato Ribeiro <joserenato2014@hotmail.com>
2024-10-15 20:58:49 -0300

    Update questao05.c

--- questao05.c
+++ questao05.c
@@ -1,26 +1,26 @@
-/* 5) Um programador construiu o seguinte código e esperava na saída o texto gostinho, mas não
- obteve essa saída. Justifique o porquê de não ter obtido o resultado esperado.
+/*9). Considerando a declaração
+int mat[4], *p, x;
+quais das seguintes expressões são válidas? Justifique.
+ p = mat + 1;
+ p = mat++;
+ p = ++mat;
+ x = (*mat);
 */
- #include <string.h>
- #include <stdio.h>
- #include <stdlib.h>
- void funcao(char** str){
-    //str++;
-    (*str)++;//Utilização de ponteiro
- }
- int main(){
-    char *str = (void *)malloc(50*sizeof(char));
-    strcpy(str, "Agostinho");
-    funcao(&str);
-    //str++; Outra forma de corrigir(e retirando a função "funcao")
-    puts(str);
-    free(str);
- return 0;
- }
 
- /*A saída de texto resulta em 'Agostinho' pois ao usar o str++ a intenção é de que ao deslocar o endereço de memória em uma posição
- ele aponte para onde está armazenado o 'g', imprimindo então apenas 'gostinho', mas essa função ao não utilizar o ponteiro, acaba por
- alterar o endereço de memória do 'str' apenas localmente, sendo assim não acontecerá nada na função original 'main'. Para corrigir isso
- é possível primeiramente apagar a função 'funcao' e adicionar o 'str++' na função main, ou então corrigir a função 'funçao' utilizando ponteiro
- de ponteiro -> (*str)++;
- */
+#include <stdio.h>
+
+int main(void){
+    int mat[4], *p, x;
+
+    p = mat + 1;//Válido e retorna o endereço de mat[1]
+
+    //p = mat++;Inválido-> Apresenta o erro "expression must be a modifiable lvalue".
+                //O nome do array é constante(não pode ser incrementado).
+
+    //p = ++mat;Inválido-> Apresenta o erro "expression must be a modifiable lvalue".
+                //Ocorre o mesmo que no caso anterior.
+   
+    x = (*mat);//Válido->Retorna o conteúdo de mat[0]
+
+    return 0;
+}
