33ab6fa2ac477602c0030018ffbafe01516a1b94
Renato Ribeiro <joserenato2014@hotmail.com>
2024-10-15 20:38:13 -0300

    Update questao05.c

--- questao05.c
+++ questao05.c
@@ -5,12 +5,14 @@
  #include <stdio.h>
  #include <stdlib.h>
  void funcao(char** str){
-    str++;
+    //str++;
+    (*str)++;//Utilização de ponteiro
  }
  int main(){
     char *str = (void *)malloc(50*sizeof(char));
     strcpy(str, "Agostinho");
     funcao(&str);
+    //str++; Outra forma de corrigir(e retirando a função "funcao")
     puts(str);
     free(str);
  return 0;
@@ -20,5 +22,5 @@
  ele aponte para onde está armazenado o 'g', imprimindo então apenas 'gostinho', mas essa função ao não utilizar o ponteiro, acaba por
  alterar o endereço de memória do 'str' apenas localmente, sendo assim não acontecerá nada na função original 'main'. Para corrigir isso
  é possível primeiramente apagar a função 'funcao' e adicionar o 'str++' na função main, ou então corrigir a função 'funçao' utilizando ponteiro
- de ponteiro -> (str**)++;
+ de ponteiro -> (*str)++;
  */
